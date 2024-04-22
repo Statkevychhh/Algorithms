2dbedae733595a4509b5c0480f93babb383d95b2
Statkevychhh <129677232+Statkevychhh@users.noreply.github.com>
2024-04-22 12:03:31 +0300

    Update Minimization_of_logic_func.py

--- Minimization_of_logic_func.py
+++ Minimization_of_logic_func.py
@@ -1,15 +1,23 @@
+from sympy.logic import SOPform, POSform
+from sympy import symbols
+
+
+
 def insertion():
     func = []
-    count = int(input('Введіть кілікість чисел для логічної функції: '))
-    for x in range(0, int(count)):
-        num = int(input(f'Введіть число {x+1}: '))
-        func.append(num)
+
+    numbers = input(f'Введіть власну функцію (наприклад: 1 2 3 4 5 6 7 8): ')
+    func = [int(num) for num in  numbers.split(' ')]
     print(f'V = {*func,}')
     return func
 
 
-def minimize(logic_func):
-    ...
+def minimize(func):
+    x1, x2, x3, x4 = symbols('x1 x2 x3 x4')
+    CDNF = SOPform([x1, x2, x3, x4], func)
+    print('СДНФ: F =', CDNF)
+    # CKNF = POSform([x1, x2, x3, x4], minterms)
+    # print('СКНФ: F =', CKNF)
 
 
 
@@ -17,16 +25,14 @@ if __name__ == '__main__':
     func = [1, 2, 3, 5, 10, 14, 15]
     
     while True:
-        n = input('''Мінімізувати логічну функцію, задану в умові завдання - введіть 1,
-Ввести логічну функцію власноруч - введіть - 2\n''')
-        if (not n.isnumeric()) or (not n in ['1','2']):
+        n = input('''Мінімізувати логічну функцію, задану в умові завдання - введіть 1,\nВвести логічну функцію власноруч - введіть - 2\n''')
+        if not n in ['1','2']:
             print('Ви ввели не валідні дані, введіть ще раз!')
             continue;
-        elif int(n) == 1:
-            print(f'V = {*func,}')
-            minimize(func)
-            break;
-        elif int(n) == 2:
-            func = insertion()
-            minimize(func)
-            break;
+        break;
+    
+    if int(n) == 2:
+        func = insertion()    
+    print(f'V = {*func,}')
+    
+    minimize(func)
